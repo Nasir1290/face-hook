3267ed9dfa91d92841a60562c29a0754024329b2
Nasir1290 <nasirmia12901290@gmail.com>
2024-03-15 13:08:23 +0600

    adding some code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegistrationPage from "./pages/RegistrationPage";
+import ProfilePage from "./pages/ProfilePage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-red-500">Hello</h1>
+    <div >
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegistrationPage />} />
+        <Route path="/me" element={<ProfilePage />} />
+        <Route path="*" element={<NotFoundPage/>} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
