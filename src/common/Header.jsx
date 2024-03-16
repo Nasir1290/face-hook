70bcef1cee2f8beb6cda118fa878d1d092b93da6
Nasir1290 <nasirmia12901290@gmail.com>
2024-03-16 16:40:34 +0600

    adding some code

---
+++ src/common/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "../assets/images/logo.svg";
+import HomeIcon from "../assets/icons/home.svg";
+import Notification from "../assets/icons/notification.svg";
+import Avatar from "../assets/images/avatars/avatar_1.png";
+const Header = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h1>How are you</h1>
+    </div>
+  );
+};
+
+export default Header;
