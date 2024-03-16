6b5f44fa29d767c97e8df464268c75fc5b4ba29c
Nasir1290 <nasirmia12901290@gmail.com>
2024-03-16 11:36:55 +0600

    adding some code

---
+++ src/common/Field.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Field = ({ label, children, htmlFor, error }) => {
+  return (
+    <div className="form-control ">
+      {label && (
+        <label htmlFor={htmlFor || getChild(child)} className=" auth-lebel">
+          {label}
+        </label>
+      )}
+      {children}
+      {!!error && (
+        <div role=" alert" className=" text-red-600">
+          {error?.message}
+        </div>
+      )}
+    </div>
+  );
+};
+
+const getChild = (children) => {
+  const child = React.Children.only(children);
+  if ("id" in child?.props) {
+    return child?.props?.id;
+  }
+};
+
+export default Field;
