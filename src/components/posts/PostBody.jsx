46de126a31620ffbc603d88c8e15ed3b20955642
Nasir1290 <nasirmia12901290@gmail.com>
2024-03-21 08:13:26 +0600

    add some code

--- src/components/posts/PostBody.jsx
+++ src/components/posts/PostBody.jsx
@@ -4,12 +4,12 @@ const PostBody = ({ poster, content }) => {
 
   return (
 
-    <div class="border-b border-[#3F3F3F] py-4 lg:py-5 lg:text-xl">
-      <div class="flex items-center justify-center overflow-hidden">
+    <div className="border-b border-[#3F3F3F] py-4 lg:py-5 lg:text-xl">
+      <div className="flex items-center justify-center overflow-hidden">
 
         {poster && (
           <img
-            class=" w-[50%] border rounded-md p-3"
+            className=" w-[50%] border rounded-md p-3"
             src={`${import.meta.env.VITE_SERVER_BASE_URL}/${poster}`}
             alt="poster"
           />
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
+import PostList from "../components/posts/PostList";
+import { initialState, postsReducer } from "../reducers/postsReducer";
+import { actions } from "../actions";
+import useAxiosAuthentication from "../hooks/useAxiosAuthentication";
 
 const HomePage = () => {
-  return <div>Home Page</div>;
+  const [state, dispatch] = useReducer(postsReducer, initialState);
+  const api = useAxiosAuthentication();
+  useEffect(() => {
+    dispatch({
+      type: actions.posts.DATA_FETCHING,
+    });
+
+    const fetchingData = async () => {
+      try {
+        const response = await api.get(
+          `${import.meta.env.VITE_SERVER_BASE_URL}/posts`
+        );
+
+        if (response.status === 200) {
+          
+          dispatch({
+            type: actions.posts.DATA_FETCHED,
+            posts: response.data,
+          });
+          console.log(response.data);
+        }
+
+      } catch (error) {
+        console.log(error);
+
+        dispatch({
+          type: actions.posts.DATA_FETCHED_EROR,
+          error: error,
+        });
+      }
+    };
+
+    fetchingData();
+  }, []);
+
+  console.log(state);
+
+  if (state.loading) {
+    return <div>Loading....</div>;
+  }
+  if (state.error) {
+    <div> OOPS !! An Error Occured !!!!</div>;
+  }
+
+  return(
+    <>
+    
+      <PostList posts={state.posts}/>
+    
+    </>
+  )
 };
 
 export default HomePage;
